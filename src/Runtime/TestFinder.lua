a6af3aca37bd2e0cea9423c756f2e717a5772f94
TheNexusAvenger <zpcook@googlemail.com>
2020-01-04 16:28:39 -0500

    Added TestFinder class

---
+++ src/Runtime/TestFinder.lua
@@ -0,0 +1,92 @@
+--[[
+TheNexusAvenger
+
+Static methods for running unit tests.
+--]]
+
+local NexusUnitTesting = require(script.Parent.Parent:WaitForChild("NexusUnitTestingProject"))
+local NexusInstance = NexusUnitTesting:GetResource("NexusInstance.NexusInstance")
+local ModuleUnitTest = NexusUnitTesting:GetResource("Runtime.ModuleUnitTest")
+
+local Runner = NexusInstance:Extend()
+Runner:SetClassName("Runner")
+
+
+
+--[[
+Returns if the given source is a unit test.
+--]]
+function Runner.ScriptContainsTests(ScriptSource)
+	--Get the require statements.
+	local RemainingScript = ScriptSource
+	while RemainingScript ~= "" do
+		--Find the next require and breeak the loop if it doesn't exist.
+		local NextRequire = string.find(RemainingScript,"require%(")
+		if not NextRequire then
+			break
+		end
+		
+		--Read the script until the end is reached.
+		RemainingScript = string.sub(RemainingScript,NextRequire + 8)
+		local CurrentParentheses = 1
+		local CurrentQuote
+		local CurrentRequire = "require("
+		while RemainingScript ~= "" do
+			local NextCharacter = string.sub(RemainingScript,1,1)
+			CurrentRequire = CurrentRequire..NextCharacter
+			RemainingScript = string.sub(RemainingScript,2)
+			
+			--Handle the next character.
+			if not CurrentQuote then
+				if NextCharacter == "(" then
+					CurrentParentheses = CurrentParentheses + 1
+				elseif NextCharacter == ")" then
+					CurrentParentheses = CurrentParentheses - 1
+				elseif NextCharacter == "'" or NextCharacter == "\"" then
+					CurrentQuote = NextCharacter
+				end
+			elseif NextCharacter == CurrentQuote then
+				CurrentQuote = nil
+			end
+			
+			--Break the loop if the end was reached.
+			if CurrentParentheses == 0 then
+				break
+			end
+		end
+		
+		--Return true if the requrie contains NexusUnitTesting or TestEZ.
+		if string.find(CurrentRequire,"[^%w]NexusUnitTesting[^%w]") or string.find(CurrentRequire,"[^%w]TestEZ[^%w]") then
+			return true
+		end
+	end
+	
+	--Return false (not a unit test).
+	return false
+end
+
+--[[
+Returns a list of tests for all of the
+ModuleScripts in the game or the given
+container instance.
+--]]
+function Runner.GetTests(Container)
+	Container = Container or game
+	
+	--Get all the tests in the game.
+	local Tests = {}
+	for _,Module in pairs(Container:GetDescendants()) do
+		pcall(function()
+			if Module:IsA("ModuleScript") and Runner.ScriptContainsTests(Module.Source) then
+				table.insert(Tests,ModuleUnitTest.new(Module))
+			end
+		end)
+	end
+	
+	--Return the tests.
+	return Tests
+end
+
+
+
+return Runner
---
+++ test/Runtime/TestFinderTests.lua
@@ -0,0 +1,71 @@
+--[[
+TheNexusAvenger
+
+Tests the TestFinder class.
+--]]
+
+local NexusUnitTesting = require("NexusUnitTesting")
+
+local NexusUnitTestingProject = require(game:GetService("ReplicatedStorage"):WaitForChild("NexusUnitTesting"):WaitForChild("NexusUnitTestingProject"))
+local TestFinder = NexusUnitTestingProject:GetResource("Runtime.TestFinder")
+
+
+
+--[[
+Tests the ScriptContainsTests method.
+--]]
+NexusUnitTesting:RegisterUnitTest("ScriptContainsTests",function(UnitTest)
+	--Assert that random scripts return false.
+	UnitTest:AssertFalse(TestFinder.ScriptContainsTests("print(\"Not a test\")"),"Script without require registers as test.")
+	UnitTest:AssertFalse(TestFinder.ScriptContainsTests("require(game.Workspace:WaitForChild(\"Module\"))"),"Script with random require registers as test.")
+	UnitTest:AssertFalse(TestFinder.ScriptContainsTests("local NexusUnitTesting = true"),"Script with NexusUnitTesting as varaibles registers as test.")
+	UnitTest:AssertFalse(TestFinder.ScriptContainsTests("local TestEZ = 5"),"Script with TestEZ as varaibles registers as test.")
+	UnitTest:AssertFalse(TestFinder.ScriptContainsTests("require(game.Workspace.NexusUnitTestingFork)"),"Script with non-Nexus Unit Testing require registers as test.")
+	UnitTest:AssertFalse(TestFinder.ScriptContainsTests("require(game.Workspace.TestEZFork)"),"Script with non-TestEZ require registers as test.")
+	
+	--Assert that unit test scripts return true.
+	UnitTest:AssertTrue(TestFinder.ScriptContainsTests("require(\"NexusUnitTesting\")"),"NexusUnitTesting test not detected.")
+	UnitTest:AssertTrue(TestFinder.ScriptContainsTests("require('NexusUnitTesting')"),"NexusUnitTesting test not detected.")
+	UnitTest:AssertTrue(TestFinder.ScriptContainsTests("require(game.TestService.NexusUnitTesting)"),"NexusUnitTesting test not detected.")
+	UnitTest:AssertTrue(TestFinder.ScriptContainsTests("require(game.TestService:WaitForChild(\"NexusUnitTesting\")"),"NexusUnitTesting test not detected.")
+	UnitTest:AssertTrue(TestFinder.ScriptContainsTests("require(game.Workspace.Module) require(game.TestService:WaitForChild(\"NexusUnitTesting\")"),"NexusUnitTesting test with other module not detected.")
+	UnitTest:AssertTrue(TestFinder.ScriptContainsTests("require(\"TestEZ\")"),"TestEZ test not detected.")
+	UnitTest:AssertTrue(TestFinder.ScriptContainsTests("require(game.TestService.TestEZ)"),"TestEZ test not detected.")
+	UnitTest:AssertTrue(TestFinder.ScriptContainsTests("require(game.TestService:WaitForChild(\"TestEZ\")"),"TestEZ test not detected.")
+end)
+	
+--[[
+Tests the GetTests method.
+--]]
+NexusUnitTesting:RegisterUnitTest("GetTests",function(UnitTest)
+	--Create the modules.
+	local Folder = Instance.new("Folder")
+	Instance.new("Part",Folder)
+	local Module1 = Instance.new("ModuleScript")
+	Module1.Name = "Module1"
+	Module1.Source = "local NexusUnitTesting = require(\"NexusUnitTesting\") return true"
+	Module1.Parent = Folder
+	local Module2 = Instance.new("ModuleScript")
+	Module2.Name = "Module2"
+	Module2.Source = "local TestEZ = require(\"TestEZ\") return true"
+	Module2.Parent = Folder
+	local Module3 = Instance.new("ModuleScript")
+	Module3.Name = "Module3"
+	Module3.Source = "local NexusUnitTesting = require(\"NexusUnitTesting\") return true"
+	Module3.Parent = Module2
+	local Module4 = Instance.new("ModuleScript")
+	Module4.Name = "Module4"
+	Module4.Source = "local NexusUnitTesting = require(\"NexusUnitTestingFork\") return true"
+	Module4.Parent = Module2
+	
+	--Assert the tests are correct.
+	local Tests = TestFinder.GetTests(Folder)
+	UnitTest:AssertEquals(#Tests,3,"Tests count is incorrect.")
+	UnitTest:AssertEquals(Tests[1].Name,"Folder.Module1","Name is incorrectt.")
+	UnitTest:AssertEquals(Tests[2].Name,"Folder.Module2","Name is incorrectt.")
+	UnitTest:AssertEquals(Tests[3].Name,"Folder.Module2.Module3","Name is incorrectt.")
+end)
+
+
+
+return true
