d33260fc65b5b25608960be5d7e14d706910b87b
TheNexusAvenger <zpcook@googlemail.com>
2020-01-04 21:29:07 -0500

    Added command line runner

--- src/init.lua
+++ src/init.lua
@@ -4,4 +4,25 @@ TheNexusAvenger
 Base module for NexusUnitTesting.
 --]]
 
-return require(script:WaitForChild("NexusUnitTestingProject"))
+local NexusUnitTesting = require(script:WaitForChild("NexusUnitTestingProject"))
+local TestFinder = NexusUnitTesting:GetResource("Runtime.TestFinder")
+
+
+
+--[[
+Runs the tests for a given container,
+or game if it isn't provided.
+Not intended to be used by custom views for
+unit tests since it doesn't provide visibility to the tests.
+--]]
+function NexusUnitTesting.RunTests(Container)
+	--Get the tests.
+	local Tests = TestFinder.GetTests(Container)
+	for _,Test in pairs(Tests) do
+		print(Test.Name)
+	end
+end
+
+
+
+return NexusUnitTesting
